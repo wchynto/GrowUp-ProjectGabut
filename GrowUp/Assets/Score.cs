fbec7141964c97cfa66aa5731264286230a106ca
w-cahyanto <68521383+w-cahyanto@users.noreply.github.com>
2021-08-11 23:40:47 +0700

    Comit 2
    Menambahkan score poin

---
+++ GrowUp/Assets/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public PlayerCollision playerCollision;
+    Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+       if (playerCollision.foodIsDestroy == true)
+        {
+            scoreText.text = "SCORE : " + playerCollision.scorePoint.ToString();
+        }
+    }
+}
--- GrowUp/Assets/Scripts/PlayerCollision.cs
+++ GrowUp/Assets/Scripts/PlayerCollision.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     float scaleUp = 0.01f;
+    public bool foodIsDestroy = false;
+    public int scorePoint;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "food")
+        if (collision.tag == "food")
         {
             Debug.Log("Kmu Makan");
             GameObject.Destroy(collision.gameObject);
             transform.localScale = new Vector2(transform.localScale.x + scaleUp, transform.localScale.y + scaleUp);
+            foodIsDestroy = true;
+            scorePoint += 1;
         }
     }
 }
